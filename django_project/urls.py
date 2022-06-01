53d8c455992139632827a4b0877852022ac00628
Fer Gonzalez <fergonzalez16@gmail.com>
2022-06-01 16:11:45 +0200

    added home

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from recipies.views import recipies
-
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("home.urls")),
     path('blog/', include('blog.urls')),
     path("recipies/", include("recipies.urls"))
 ]
